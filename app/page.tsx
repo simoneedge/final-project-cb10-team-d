b3ea5345594c6820a4fa7deae07ae9a94e9668cb
Adriana <piccioneadriana@gamil.com>
2024-09-11 21:40:28 +0200

    Feat: edit page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ const HomePage = () => {
 
   return (
     <div className="flex flex-col justify-between items-center min-h-screen bg-gray-100 relative">
-      <NavBar />
+      {/* <NavBar /> */}
       <main className="flex flex-col items-center justify-center flex-grow space-y-4">
         <Button />
         {errorMessage && <p className="text-red-500">{errorMessage}</p>}
