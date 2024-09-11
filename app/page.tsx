0776ac76f34be6529b38b4c1b40913304f63d46c
Adriana <piccioneadriana@gamil.com>
2024-09-11 21:30:41 +0200

    Feat:edit page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,9 +35,9 @@ const HomePage = () => {
 
   return (
     <div className="flex flex-col justify-between items-center min-h-screen bg-gray-100 relative">
-      {/* <NavBar /> */}
+      <NavBar />
       <main className="flex flex-col items-center justify-center flex-grow space-y-4">
-        <Button/>
+        <Button />
         {errorMessage && <p className="text-red-500">{errorMessage}</p>}
         {events.length > 0 ? (
           events.map((event, index) => (
