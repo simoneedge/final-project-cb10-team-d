6164952e578f38f00975442354517baee11dbb9e
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-09-13 20:30:05 +0200

    add € on price

--- app/activities/[id]/page.tsx
+++ app/activities/[id]/page.tsx
@@ -77,7 +77,7 @@ const EventDetailPage = async ({ params }: { params: { id: string } }) => {
               <strong className="text-xl font-titolo mb-4 text-rosso">
                 Prezzo:{" "}
               </strong>
-              {event.price === "0" ? "Ingresso gratuito" : ` ${event.price}`}
+              {event.price === "0" ? "Ingresso gratuito" : ` ${event.price}€`}
             </p>
             <p>
               <strong className="text-xl font-titolo mb-4 text-rosso">
