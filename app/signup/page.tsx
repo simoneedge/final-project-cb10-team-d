1115dc40ea8ed2879280f3cd241848ed843effbd
Adriana <piccioneadriana@gamil.com>
2024-09-13 12:33:24 +0200

    feat: update page.tsx file in Signup

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,44 +1,52 @@
 'use client';
 
-import { useState } from "react";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { useRouter } from "next/navigation";
-import { auth } from "@/firebaseconfig";
+import { useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
+import { auth } from '@/firebaseconfig';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function SignUp() {
   const router = useRouter();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPass, setConfirmPass] = useState("");
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPass, setConfirmPass] = useState('');
   const [error, setError] = useState<string | null>(null);
 
-  const emailValidation = !email.trim();
-  const passwordValidation = !password.trim();
-  const confirmPasswordValidation = !confirmPass.trim();
-  const arePasswordAndConfirmPassEqual = password === confirmPass;
-
   const formValidation =
-    emailValidation ||
-    passwordValidation ||
-    confirmPasswordValidation ||
-    !arePasswordAndConfirmPassEqual;
+    !firstName.trim() ||
+    !lastName.trim() ||
+    !email.trim() ||
+    !password.trim() ||
+    !confirmPass.trim() ||
+    password !== confirmPass;
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (formValidation) {
-      setError("Please fill out all fields correctly.");
+      setError('Please fill out all fields correctly.');
       return;
     }
-    
+
     try {
       await createUserWithEmailAndPassword(auth, email, password);
-      router.push("/signin");
+
+      
+      toast.success('Registrazione effettuata con successo!');
+
+      // Reinderizza nella home "/" dopo 2 secondi
+      setTimeout(() => {
+        router.push('/'); 
+      }, 2000);
     } catch (error) {
       if (error instanceof Error) {
         setError(error.message);
       } else {
-        setError('An unknown error occurred.');
+        setError('Error.');
       }
     }
   };
@@ -46,43 +54,118 @@ export default function SignUp() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-50 p-4">
       <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-6">
-        <h1 className="text-2xl font-semibold text-gray-800 mb-6">Sign Up</h1>
+        <h1 className="text-2xl font-semibold text-gray-800 mb-6">Registration form</h1>
         {error && (
           <div className="mb-4 p-4 border-l-4 border-red-500 bg-red-50 text-red-700 rounded">
             <strong className="font-semibold">Error:</strong>
             <p>{error}</p>
           </div>
         )}
-        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-          <input
-            type="email"
-            className="border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            className="border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <input
-            type="password"
-            className="border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            placeholder="Confirm Password"
-            value={confirmPass}
-            onChange={(e) => setConfirmPass(e.target.value)}
-          />
-          <button
-            className={`bg-indigo-600 hover:bg-indigo-700 text-white p-3 rounded-lg transition-colors duration-300 ${formValidation ? "opacity-50 cursor-not-allowed" : ""}`}
-            disabled={formValidation}
-            type="submit"
-          >
-            Sign Up
-          </button>
+        <form className="space-y-6" onSubmit={handleSubmit}>
+          <div className="space-y-8">
+            <div className="border-b border-gray-900/10 pb-8">
+              <div className="mt-8 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="sm:col-span-3">
+                  <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                    First Name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      id="first-name"
+                      name="first-name"
+                      value={firstName}
+                      onChange={(e) => setFirstName(e.target.value)}
+                      className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-indigo-600 sm:text-sm"
+                      placeholder="First Name"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
+                    Last Name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      id="last-name"
+                      name="last-name"
+                      value={lastName}
+                      onChange={(e) => setLastName(e.target.value)}
+                      className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-indigo-600 sm:text-sm"
+                      placeholder="Last Name"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-6">
+                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                    Email
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="email"
+                      id="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-indigo-600 sm:text-sm"
+                      placeholder="Email Address"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                    Password
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="password"
+                      id="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-indigo-600 sm:text-sm"
+                      placeholder="Password"
+                    />
+                  </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label htmlFor="confirm-password" className="block text-sm font-medium leading-6 text-gray-900">
+                    Confirm Password
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="password"
+                      id="confirm-password"
+                      value={confirmPass}
+                      onChange={(e) => setConfirmPass(e.target.value)}
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-indigo-600 sm:text-sm"
+                      placeholder="Confirm Password"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex items-center justify-end gap-x-6">
+              <button type="button" className="text-sm font-semibold leading-6 text-gray-900" onClick={() => router.push('/')}>
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className={`rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus:ring-2 focus:ring-indigo-600 ${
+                  formValidation ? 'opacity-50 cursor-not-allowed' : ''
+                }`}
+                disabled={formValidation}
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
         </form>
+        <ToastContainer />
       </div>
     </div>
   );
