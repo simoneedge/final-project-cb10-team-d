295b2df325e9c7d0e4a4ac6dae990ccc77d130c2
Ania <beninatistefania@gmail.com>
2024-09-16 21:17:13 +0200

    feat: cambio cuore senza macchia b

--- src/components/HeartButton.tsx
+++ src/components/HeartButton.tsx
@@ -62,14 +62,14 @@ const HeartButton: React.FC<HeartButtonProps> = ({ eventId, color, title, image,
       }}
       title="Heart Button"
     >
-      <svg
-        className="w-4 h-4 text-white"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M3.172 5.172a4 4 0 015.656 0L10 6.344l1.172-1.172a4 4 0 115.656 5.656L10 18.828l-6.828-6.828a4 4 0 010-5.656z"></path>
-      </svg>
+     <svg
+  className="w-6 h-6 text-white"
+  fill="currentColor"
+  viewBox="0 0 24 24"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+</svg>
     </button>
   );
 };
