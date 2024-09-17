2a70f2445d0d267332ae47e2cd33a7995917ec04
Maryscib1997 <mariadomenicascibilia97@gmail.com>
2024-09-17 11:18:19 +0200

    page about

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -8,17 +8,17 @@ interface SwitchProps {
 const Switch: React.FC<SwitchProps> = ({ active, setActive }) => {
   return (
     <div
-      className={`relative w-16 h-8 rounded-full cursor-pointer transition-colors duration-300 ${active ? "bg-rosso" : "bg-gray-300"}`}
+      className={`relative w-16 h-8 rounded-full cursor-pointer transition-colors duration-300 ${active ? "bg-rosso" : "bg-giallo"}`}
       onClick={() => setActive(!active)}
     >
       {/* Background */}
       <div
-        className={`absolute inset-0 rounded-full transition-all duration-300 ${active ? "bg-rosso" : "bg-gray-300"}`}
+        className={`absolute inset-0 rounded-full transition-all duration-300 ${active ? "bg-rosso" : "bg-giallo"}`}
       />
       
       {/* Circle */}
       <div
-        className={`absolute top-[2px] left-1 w-7 h-7 rounded-full shadow transition-transform duration-300 ${active ? "bg-white translate-x-8" : "bg-rosso"}`}
+        className={`absolute top-[2px] left-1 w-7 h-7 rounded-full shadow transition-transform duration-300 ${active ? "bg-giallo translate-x-8" : "bg-rosso"}`}
       />
     </div>
   );
