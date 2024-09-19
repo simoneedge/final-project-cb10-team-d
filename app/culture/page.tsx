eb0775e7100144279ad07e8eb89879a42fd1f9f2
NunzioBasciano <nbasho1988@gmail.com>
2024-09-20 00:52:57 +0200

    fix: culture

--- app/culture/page.tsx
+++ app/culture/page.tsx
@@ -12,93 +12,51 @@ import Loading from "@/src/components/Loading";
 import CategoryBanner from "@/src/components/CategoryBanner";
 import { getAuth } from "firebase/auth";
 
-const fetchData = async (
-): Promise<{ events: IEvent[] }> => {
+const fetchData = async (): Promise<{ events: IEvent[] }> => {
   try {
-    const res = await fetch(`/api/events`, {
-      cache: "no-cache",
-    });
+    const res = await fetch(`/api/events`, { cache: "no-cache" });
     if (!res.ok) {
       throw new Error(`HTTP error! status: ${res.status}`);
     }
-    const data = await res.json();
-    return data;
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.error("Error fetching data:", error.message);
-      throw new Error(error.message);
-    } else {
-      throw new Error("Unknown error occurred");
-    }
+    return await res.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw new Error("Failed to fetch data.");
   }
 };
 
 export default function CulturePage() {
   const [cultures, setCultures] = useState<IEvent[]>([]);
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [filteredEvents, setFilteredEvents] = useState<IEvent[]>([]);
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [isFree, setIsFree] = useState<boolean>(false);
   const [today, setToday] = useState<number>(0);
-  const [startNextWeek, setStartNextWeek] = useState<number | undefined>(
-    undefined
-  );
+  const [startNextWeek, setStartNextWeek] = useState<number | undefined>(undefined);
   const [endNextWeek, setEndNextWeek] = useState<number | undefined>(undefined);
   const [loading, setLoading] = useState<boolean>(true);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [favoriteEventTitle, setFavoriteEventTitle] = useState<string[]>([]);
 
-  // Funzione per recuperare i preferiti dell'utente
-  const fetchFavorites = async (email: string | null) => {
-    try {
-      const response = await fetch(`/api/profiles?email=${email}`);
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const data = await response.json();
-      const favoriteTitle = data.profile.events.map(
-        (event: { title: string }) => event.title
-      );
-      setFavoriteEventTitle(favoriteTitle);
-    } catch (error) {
-      console.error("Errore nel recupero dei preferiti:", error);
-    }
-  };
-
-<<<<<<< HEAD
-=======
-  // Stato per la paginazione
-  const [currentPage, setCurrentPage] = useState<number>(1);
-  const [totalPages, setTotalPages] = useState<number>(1);
-  const limit = 10;
->>>>>>> 6ec421450b1ef9c659fafa3f7868906d7efb83fe
-
-  // Effetto per caricare i dati iniziali
   useEffect(() => {
     const loadData = async () => {
       setLoading(true);
       try {
-<<<<<<< HEAD
         const data = await fetchData();
         setCultures(data.events);
         setFilteredEvents(data.events);
-        // Recupera i preferiti se l'utente è autenticato
-=======
-        const data = await fetchData(currentPage, limit);
-        setCultures(data.cultures);
-        setFilteredEvents(data.cultures);
-        setTotalPages(data.totalPages);
->>>>>>> 6ec421450b1ef9c659fafa3f7868906d7efb83fe
+
         const auth = getAuth();
         const user = auth.currentUser;
 
         if (user) {
           const userEmail = user.email;
-          await fetchFavorites(userEmail);
+          const response = await fetch(`/api/profiles?email=${userEmail}`);
+          const profileData = await response.json();
+          const favoriteTitles = profileData.profile.events.map((event: { title: string }) => event.title);
+          setFavoriteEventTitle(favoriteTitles);
         }
-      } catch (error: unknown) {
-        if (error instanceof Error) {
-          setErrorMessage("Failed to load data.");
-        }
+      } catch (error) {
+        setErrorMessage("Failed to load data.");
       } finally {
         setLoading(false);
       }
@@ -107,29 +65,59 @@ export default function CulturePage() {
     loadData();
   }, []);
 
-  // Funzione per gestire la ricerca
+  useEffect(() => {
+    const applyFilters = () => {
+      let filtered = cultures;
+
+      if (searchQuery) {
+        filtered = filtered.filter(
+          (event) =>
+            event.title?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            event.location?.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            event.tag?.some((tag) => tag.toLowerCase().includes(searchQuery.toLowerCase()))
+        );
+      }
+
+      if (isFree) {
+        filtered = filtered.filter((event) => event.price === "0");
+      }
+
+      if (today) {
+        filtered = filtered.filter((event) => {
+          const startEvent = event.dateStart ? getDayOfYear(event.dateStart) : -1;
+          const endEvent = event.dateEnd ? getDayOfYear(event.dateEnd) : -1;
+          return today >= startEvent && today <= endEvent;
+        });
+      }
+
+      if (startNextWeek !== undefined && endNextWeek !== undefined) {
+        filtered = filtered.filter((event) => {
+          const startEvent = event.dateStart ? getDayOfYear(event.dateStart) : -1;
+          const endEvent = event.dateEnd ? getDayOfYear(event.dateEnd) : -1;
+          return startEvent <= endNextWeek && endEvent >= startNextWeek;
+        });
+      }
+
+      setFilteredEvents(filtered);
+    };
+
+    applyFilters();
+  }, [cultures, searchQuery, isFree, today, startNextWeek, endNextWeek]);
+
   const handleSearch = (query: string) => {
     setSearchQuery(query);
-    applyFilters(query, isFree, today);
   };
 
-  // Filtra eventi di oggi
   const handleTodayClick = () => {
-    const date = formattedDate();
-    const dayOfYear = getDayOfYear(date);
+    const dayOfYear = getDayOfYear(formattedDate());
     setToday(dayOfYear);
-    applyFilters(searchQuery, isFree, dayOfYear);
   };
 
-  // Filtra eventi di domani
   const handleTomorrowClick = () => {
-    const date = formattedDate(1);
-    const dayOfYear = getDayOfYear(date);
+    const dayOfYear = getDayOfYear(formattedDate(1));
     setToday(dayOfYear);
-    applyFilters(searchQuery, isFree, dayOfYear);
   };
 
-  // Filtra eventi della prossima settimana
   const handleNextWeekClick = () => {
     const today = new Date();
     const dayOfWeek = today.getDay();
@@ -138,93 +126,16 @@ export default function CulturePage() {
     const nextSunday = nextMonday + 6;
     setStartNextWeek(nextMonday);
     setEndNextWeek(nextSunday);
-    applyFilters(searchQuery, isFree, 0, nextMonday, nextSunday);
   };
 
-  // Funzione per applicare i filtri agli eventi
-  const applyFilters = (
-    query: string,
-    isFree: boolean,
-    dayOfYear: number,
-    startNextWeek?: number,
-    endNextWeek?: number
-  ) => {
-    let filtered = cultures;
-
-    filtered = filtered.filter(
-      (event) =>
-        Boolean(event.reviewed) === true || event.reviewed === undefined
-    );
-    filtered = filtered.filter(
-      (event) => event.color === '#4E614E'
-    )
-    // Filtro per la query di ricerca
-    if (query !== "") {
-      filtered = filtered.filter(
-        (event) =>
-          event.title?.toLowerCase().includes(query.toLowerCase()) ||
-          event.location?.toLowerCase().includes(query.toLowerCase()) ||
-          event.tag?.some((tag) =>
-            tag.toLowerCase().includes(query.toLowerCase())
-          )
-      );
-    }
-
-    // Filtro per eventi gratuiti
-    if (isFree) {
-      filtered = filtered.filter((event) => event.price === "0");
-    } else {
-      filtered = filtered.filter((event) => event.price !== "0");
-    }
-
-    // Filtro per il giorno specifico
-    if (dayOfYear) {
-      filtered = filtered.filter((event) => {
-        const startEvent = event.dateStart ? getDayOfYear(event.dateStart) : -1;
-        const endEvent = event.dateEnd ? getDayOfYear(event.dateEnd) : -1;
-        return dayOfYear >= startEvent && dayOfYear <= endEvent;
-      });
-    }
-
-    // Filtro per la prossima settimana
-    if (startNextWeek !== undefined && endNextWeek !== undefined) {
-      filtered = filtered.filter((event) => {
-        const startEvent = event.dateStart ? getDayOfYear(event.dateStart) : -1;
-        const endEvent = event.dateEnd ? getDayOfYear(event.dateEnd) : -1;
-        return startEvent <= endNextWeek && endEvent >= startNextWeek;
-      });
-    }
-
-    setFilteredEvents(filtered);
-  };
-
-  // Applica i filtri quando gli stati cambiano
-  useEffect(() => {
-    applyFilters(searchQuery, isFree, today, startNextWeek, endNextWeek);
-  }, [cultures, searchQuery, isFree, today, startNextWeek, endNextWeek]);
-
-<<<<<<< HEAD
-=======
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const handlePreviousPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
   const handleResetFilters = () => {
     setSearchQuery("");
     setIsFree(false);
     setToday(0);
     setStartNextWeek(undefined);
     setEndNextWeek(undefined);
-    setFilteredEvents(cultures); // Reset events
+    setFilteredEvents(cultures);
   };
->>>>>>> 6ec421450b1ef9c659fafa3f7868906d7efb83fe
 
   return (
     <div className="flex flex-col justify-between items-center min-h-screen bg-gray-100 relative">
@@ -242,7 +153,6 @@ export default function CulturePage() {
             onResetFilters={handleResetFilters}
           />
         </div>
-<<<<<<< HEAD
         <div className="card-container grid grid-cols-1 md:grid-cols-3 gap-4 lg:gap-8 justify-items-center items-start">
           {errorMessage && <p className="text-red-500">{errorMessage}</p>}
           {loading ? (
@@ -251,14 +161,13 @@ export default function CulturePage() {
             filteredEvents.map((culture, index) => (
               <div
                 key={culture._id || index}
-                className="col-span-1 w-full md:w-auto  justify-center transform hover:scale-105 transition-transform duration-300 custom-shadow" // Mantieni 'flex justify-center' qui
+                className="col-span-1 w-full md:w-auto  justify-center transform hover:scale-105 transition-transform duration-300 custom-shadow"
               >
                 <Card
                   dateEnd={culture.dateEnd}
                   dateStart={culture.dateStart}
                   price={culture.price}
                   eventId={culture._id}
-                  key={culture._id || index}
                   backgroundColor="#4E614E"
                   title={culture.title || "No title available"}
                   imageSrc={culture.image || "default-image-url"}
@@ -267,14 +176,8 @@ export default function CulturePage() {
                       <ArrowButton />
                     </Link>
                   }
-                  isLiked={
-                    culture.title
-                      ? favoriteEventTitle.includes(culture.title)
-                      : false
-                  }
-                  onHeartClick={() =>
-                    fetchFavorites(getAuth().currentUser?.email || "")
-                  }
+                  isLiked={culture.title ? favoriteEventTitle.includes(culture.title) : false}
+                  onHeartClick={() => fetchFavorites(getAuth().currentUser?.email || "")}
                 />
               </div>
             ))
@@ -282,67 +185,6 @@ export default function CulturePage() {
             <p className="justify-items-center">No events found...</p>
           )}
         </div>
-
-=======
-        {loading ? (
-          <Loading />
-        ) : (
-          <div className="card-container grid grid-cols-1 md:grid-cols-3 gap-4 lg:gap-8 justify-items-center items-start">
-            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-            {filteredEvents.length > 0 ? (
-              filteredEvents.map((culture, index) => (
-                <div
-                  key={culture._id || index}
-                  className="col-span-1 w-full md:w-auto  justify-center transform hover:scale-105 transition-transform duration-300 custom-shadow" // Mantieni 'flex justify-center' qui
-                >
-                  <Card
-                    eventId={culture._id}
-                    key={culture._id || index}
-                    backgroundColor="#4E614E"
-                    title={culture.title || "No title available"}
-                    imageSrc={culture.image || "default-image-url"}
-                    link={
-                      <Link href={`/culture/${culture._id}`}>
-                        <ArrowButton />
-                      </Link>
-                    }
-                    isLiked={
-                      culture.title
-                        ? favoriteEventTitle.includes(culture.title)
-                        : false
-                    }
-                    onHeartClick={() =>
-                      fetchFavorites(getAuth().currentUser?.email || "")
-                    }
-                  />
-                </div>
-              ))
-            ) : (
-              <p className="justify-items-center">No events found...</p>
-            )}
-          </div>
-        )}
-
-        <div className="pagination-controls flex justify-center m-10">
-          <button
-            onClick={handlePreviousPage}
-            disabled={currentPage === 1}
-            className="mr-4 w-32 px-4 py-2 bg-gray-700 text-white rounded disabled:bg-gray-300 disabled:text-gray-500 disabled:opacity-50 text-center"
-          >
-            Previous
-          </button>
-          <span className="text-center px-4 py-2 text-gray-700 font-medium">
-            {currentPage} of {totalPages}
-          </span>
-          <button
-            onClick={handleNextPage}
-            disabled={currentPage === totalPages}
-            className="ml-4 w-32 px-4 py-2 bg-gray-700 text-white rounded disabled:bg-gray-300 disabled:text-gray-500 disabled:opacity-50 text-center"
-          >
-            Next
-          </button>
-        </div>
->>>>>>> 6ec421450b1ef9c659fafa3f7868906d7efb83fe
       </div>
     </div>
   );
