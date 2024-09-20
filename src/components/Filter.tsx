6a76b63804f44f8829340d9ba46fa3f3c6c5fc1f
Adriana <piccioneadriana@gamil.com>
2024-09-20 22:59:22 +0200

    Feat: modifiche mobile filter e card

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -43,25 +43,22 @@ function Filter({
   };
 
   return (
-    <div className="container mx-auto flex flex-col items-center gap-4 p-4 md:px-20 mb-10 mt-10">
-      {/* Campo di ricerca */}
-      <div className="w-full md:w-1/3 mb-4">
-        <Search
-          query={query}
-          onSearch={onSearch}
-          onReset={() => onSearch("")}
-          className="px-4 py-2 w-full" // Gestione padding per input di ricerca
-        />
-      </div>
+    <div className="container mx-auto flex flex-col items-center gap-4 p-4 lg:px-20 mb-10 mt-10">
+      <div className="w-full flex flex-col gap-4 items-center justify-center lg:flex-row lg:gap-6">
+        {/* Campo di ricerca */}
+        <div className="w-full lg:w-1/3 md:w-96">
+          <Search
+            query={query}
+            onSearch={onSearch}
+            onReset={() => onSearch("")}
+          />
+        </div>
 
-      {/* Filtri */}
-      <div className="w-full flex flex-col gap-4 items-center">
-        {/* Pulsanti temporali */}
-        <div className="flex flex-col md:flex-row gap-4 w-full">
+        <div className="flex flex-col lg:flex-row gap-4 w-full lg:w-auto justify-center md:w-96">
           <Button
             label={"Oggi"}
             onClick={() => toggleButton("oggi", onTodayClick)}
-            className={`w-full px-4 py-2 border-2 ${
+            className={`w-full lg:w-auto px-4 py-2 border-2 ${
               activeButton === "oggi"
                 ? "bg-rosso text-white"
                 : "border-rosso text-rosso bg-white hover:bg-rosso hover:text-white"
@@ -70,7 +67,7 @@ function Filter({
           <Button
             label={"Domani"}
             onClick={() => toggleButton("domani", onTomorrowClick)}
-            className={`w-full px-4 py-2 border-2 ${
+            className={`w-full lg:w-auto px-4 py-2 border-2 ${
               activeButton === "domani"
                 ? "bg-rosso text-white"
                 : "border-rosso text-rosso bg-white hover:bg-rosso hover:text-white"
@@ -79,7 +76,7 @@ function Filter({
           <Button
             label={"Prossima settimana"}
             onClick={() => toggleButton("prossima", onNextWeekClick)}
-            className={`w-full px-4 py-2 border-2 ${
+            className={`w-full lg:w-auto px-4 py-2 border-2 ${
               activeButton === "prossima"
                 ? "bg-rosso text-white"
                 : "border-rosso text-rosso bg-white hover:bg-rosso hover:text-white"
@@ -88,19 +85,18 @@ function Filter({
         </div>
 
         {/* SwitchBox e pulsante Reset Filtri */}
-        <div className="flex flex-row justify-between items-center gap-4 w-full md:w-auto">
+        <div className="flex flex-col lg:flex-row items-center gap-4 w-full lg:w-auto justify-center">
           <div className="flex items-center border-2 border-rosso text-rosso bg-white px-3 py-2">
             <SwitchBox
               label={"Gratis"}
               value={isFree}
               setValue={setIsFree}
-              className="text-sm p-1" // Riduzione della dimensione del testo e del padding per lo switch
             />
           </div>
           <Button
             label={"Reset Filtri"}
             onClick={handleReset}
-            className="w-auto px-4 py-2 border-2 border-gray-500 text-gray-500 bg-white hover:bg-gray-500 hover:text-white font-bold transition-colors duration-300"
+            className=" md:w-96 w-full lg:w-auto px-4 py-2 border-2 border-gray-500 text-gray-500 bg-white hover:bg-gray-500 hover:text-white font-bold transition-colors duration-300"
           />
         </div>
       </div>
