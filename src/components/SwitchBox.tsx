3b5a83f0c50882d5b3b779082500e9afa0af70e1
Maryscib1997 <mariadomenicascibilia97@gmail.com>
2024-09-22 16:38:33 +0200

    style 22 settembre

--- src/components/SwitchBox.tsx
+++ src/components/SwitchBox.tsx
@@ -17,12 +17,12 @@ const SwitchBox = (props: SwitchBoxProps) => {
     <div className="flex items-center gap-3">
       <h3 className="text-gray-800 text-sm font-bold">{label}</h3>
       <div
-        className={`relative inline-flex items-center h-8 w-20 cursor-pointer rounded-full transition-colors duration-300 ${value ? "bg-rosso" : "bg-gray-300"
+        className={`relative inline-flex items-center h-7 w-16 cursor-pointer rounded-full transition-colors duration-300 ${value ? "bg-rosso" : "bg-gray-300"
           } ${value ? "hover:bg-rosso-dark" : "hover:bg-gray-400"}`} // Cambia leggermente il colore al passaggio del mouse
         onClick={() => setValue(!value)}
       >
         <span
-          className={`inline-block w-8 h-8 transform rounded-full bg-white shadow-md transition-transform duration-300 ease-in-out ${value ? "translate-x-12" : "translate-x-0"
+          className={`inline-block w-7 h-7 transform rounded-full bg-white shadow-md transition-transform duration-300 ease-in-out ${value ? "translate-x-10" : "translate-x-0"
             }`}
         />
       </div>
