27d0cc860724489bd87ad5f5e81c2e4e6466175d
NunzioBasciano <165208510+NunzioBasciano@users.noreply.github.com>
2024-09-23 14:38:40 +0200

    Revert "Test"

--- app/propose/page.tsx
+++ app/propose/page.tsx
@@ -7,32 +7,11 @@ import "react-toastify/dist/ReactToastify.css";
 import Modal from "@/src/components/Modal";
 import Image from "next/image";
 
-function formatArticle(article: string, setTitles: (titles: string[]) => void, setParagraphs: (paragraphs: string[]) => void) {
-  if (article) {
-    const lines = article.split("\n");
-    const titles: string[] = [];
-    const paragraphs: string[] = [];
-
-    for (const line of lines) {
-      const trimmedLine = line.trim();
-      if (trimmedLine.startsWith("##")) {
-        titles.push(trimmedLine.replace("##", "").trim());
-      } else if (trimmedLine.length > 0) {
-        paragraphs.push(trimmedLine);
-      }
-    }
-
-    setTitles(titles);
-    setParagraphs(paragraphs);
-  }
-}
-
 // Funzione per ottenere immagini da Pexels
 const fetchPexelsImage = async (keywords: string[]): Promise<string> => {
   const accessKey = process.env.NEXT_PUBLIC_PEXELS_KEY || ""; // Chiave API Pexels
   const query = keywords.join("+");
   const url = `https://api.pexels.com/v1/search?query=${query}&per_page=1`;
-  const images: string[] = [];
 
   try {
     const response = await fetch(url, {
@@ -69,8 +48,6 @@ export default function ProposePage() {
   });
 
   const [article, setArticle] = useState<string>(""); // Stato per l'articolo generato
-  const [titles, setTitles] = useState<string[]>([]);
-  const [paragraphs, setParagraphs] = useState<string[]>([]);
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
   const [isGeneratingArticle, setIsGeneratingArticle] =
@@ -127,17 +104,13 @@ export default function ProposePage() {
       formData.location,
       ...formData.tag,
     ];
-
     const imageUrl = await fetchPexelsImage(keywords);
-
-    const articleKeywords = [...titles, ...paragraphs].slice(0, 5);
 
     const finalFormData = {
       ...formattedData,
       image: imageUrl || formData.image,
       color,
       reviewed: false,
-      article
     };
 
     setIsSubmitting(true);
